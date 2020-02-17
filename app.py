23fb8940ee2d70b9a35fabb75d51799720dd8757
wade <wadechen@outlook.com>
2020-02-17 17:38:44 +0900

    update ssinfo

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 import os
 import json
+import re
 
 app = Flask(__name__)
 loader = FileSystemLoader('templates')
@@ -13,6 +14,43 @@ def index():
     template = env.get_template('index.html')
     result = template.render()
     return result
+
+
+@app.route('/ssinfo')
+def ssinfo():
+    # ssconfiglist = []
+    # ssservices = os.popen('ls /etc/systemd/system | grep shadowsocks-').read().split('\n')
+    # for ssservice in ssservices:
+    #     servicestatus = os.popen('systemctl status ' + ssservice).read()
+    #     with open('/etc/shadowsocks-' + re.search('(?<=shadowsocks-).*(?=\.service)', ssservice) + '.json', 'r') as f:
+    #         ssconfig = json.load(f)
+    #     activestatus = re.search('(?<=Active: )(active|inactive)', servicestatus).group()
+    #     ssconfiglist.append({
+    #         'IP': '23.105.207.48',
+    #         'port': ssconfig['server_port'],
+    #         'password': ssconfig['password'],
+    #         'encryption': ssconfig['method'],
+    #         'status': activestatus,
+    #     })
+
+    ssconfiglist = [
+        {
+            'IP': '23.105.207.48',
+            'port': 'test',
+            'password': 'test',
+            'encryption': 'test',
+            'status': 'active',
+        },
+        {
+            'IP': '23.105.207.48',
+            'port': 'test',
+            'password': 'test',
+            'encryption': 'test',
+            'status': 'inactive',
+        },
+    ]
+
+    return json.dumps(ssconfiglist)
 
 
 @app.route('/add', methods=['post'])
@@ -38,7 +76,8 @@ def add():
         'WantedBy=multi-user.target\n'
     ]
 
-    with open('C:/temp' + ssconfigfilepath, 'w') as ssconfigfile, open('C:/temp' + ssserviceconfigfilepath, 'w') as ssserviceconfigfile:
+    with open('C:/temp' + ssconfigfilepath, 'w') as ssconfigfile, open('C:/temp' + ssserviceconfigfilepath,
+                                                                       'w') as ssserviceconfigfile:
         ssconfigfile.writelines(json.dumps(ssconfig))
         ssserviceconfigfile.writelines(ssserviceconfig)
 
