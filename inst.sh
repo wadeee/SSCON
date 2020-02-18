34715ddd0e5e7b6c3540f60d366319624a1205a3
wade <wadechen@outlook.com>
2020-02-18 15:25:37 +0900

    modify the config and shell

--- inst.sh
+++ inst.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#pip install
 pip install flask
 pip install jinja2
 pip install gunicorn
+
+#add sscon to service
+echo y | cp ./sscon.service /etc/systemd/system/
+systemctl systemctl daemon-reload
+systemctl enable sscon
+ssytemctl start sscon
