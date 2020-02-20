b9ac2dca46f4ae9064a5e517cec246d99bd35aa9
wade <wadechen@outlook.com>
2020-02-20 15:06:33 +0900

    modify the nginx config and shell

--- inst.sh
+++ inst.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-#pip install
+## pip install ##
 pip install flask
 pip install jinja2
 pip install gunicorn
 
-#add sscon to service
+## add sscon to service ##
 echo y | cp ./sscon.service /etc/systemd/system/
 systemctl systemctl daemon-reload
 systemctl enable sscon
 systemctl start sscon
+
+## add config to nginx ##
+echo y | cp ./sscon.nginx.http.conf /etc/nginx/conf.d/
+systemctl restart nginx
