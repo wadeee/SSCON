b67249330faf2c8bc7298edf64283490d060aac3
wade <wadechen@outlook.com>
2020-02-20 15:11:19 +0900

    modify install shell

--- inst.sh
+++ inst.sh
@@ -7,7 +7,7 @@ pip install gunicorn
 
 ## add sscon to service ##
 echo y | cp ./sscon.service /etc/systemd/system/
-systemctl systemctl daemon-reload
+systemctl daemon-reload
 systemctl enable sscon
 systemctl start sscon
 
